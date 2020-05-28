9f0870699eb66aa3a245e4101fa4293a08be6523
Philip Ullersted <philipullersted@gmail.com>
2020-05-28 18:22:55 +0200

    Added logs

--- app/ovpn/get-ovpn-files.sh
+++ app/ovpn/get-ovpn-files.sh
@@ -4,15 +4,19 @@
 
 echo "$(adddate) INFO: Download and extract ovpn files"
 
+echo "$(adddate) INFO: Create directory if no volume is done"
 #Create directory if no volume is done
 mkdir -p /app/ovpn/config
 
+echo "$(adddate) INFO: First remove files if exists"
 #First remove files if exists
 rm -rf ${OVPN_CONFIG_DIR}/ovpn*
 
+echo "$(adddate) INFO: Curl download ovpn files from NordVPN"
 #Curl download ovpn files from NordVPN
 curl -s -o ${OVPN_CONFIG_DIR}/ovpn.zip ${OVPN_FILES}
 
+echo "$(adddate) INFO: Unzip files"
 #Unzip files
 unzip -q ${OVPN_CONFIG_DIR}/ovpn.zip -d ${OVPN_CONFIG_DIR}
 
@@ -23,5 +27,6 @@ else
     echo "$(adddate) ERROR: OVPN files unzipped failed!"
 fi
 
+echo "$(adddate) INFO: Remove current zip"
 #Remove current zip
 rm -rf ${OVPN_CONFIG_DIR}/ovpn.zip
